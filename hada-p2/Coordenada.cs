a655057dbd03550fccc6904865eb8f3bea784703
Ruben <ral44@alu.ua.es>
2026-02-23 14:19:59 +0100

    06289193G Coordenada constructores

---
+++ hada-p2/Coordenada.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hada_p2
+{
+    internal class Coordenada
+    {
+        int fila = 0;
+        int columna = 0;
+        public Coordenada()
+        {
+            fila = 0;
+            columna = 0;
+        }
+        public Coordenada(int fila , int columnsa)
+        {
+            this.fila = fila;
+            this.columna = columnsa;
+        }
+        public Coordenada (string fila , string columna)
+        {
+            this.fila=int.Parse(fila);
+            this.columna=int.Parse(columna); ;
+
+        }
+        public Coordenada(Coordenada other)
+        {
+            this.fila = other.fila;
+            this.columna = other.columna;
+        }
+    }
+}
